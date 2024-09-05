69f5d4da000c876f91d481ad73cd3a59a4972ba5
Renan <renanclaude.rcg@gmail.com>
2024-09-05 18:08:34 -0300

    Updating index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,17 @@
 import Questionnaire from "@/components/Questionnaire";
-import AlternativeModel from "@/model/AlternativeModel";
 import QuestionModel from "@/model/QuestionModel";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-const questionMock = new QuestionModel(1, "Melhor cor?", [
-  AlternativeModel.incorrect("Vermelho"),
-  AlternativeModel.incorrect("Verde"),
-  AlternativeModel.incorrect("Amarelo"),
-  AlternativeModel.correct("Azul")
-]);
 
 const BASE_URL = "http://localhost:3000/api";
 
 
 export default function Home() {
-  const [question, setQuestion] = useState<QuestionModel>(questionMock);
+  const [question, setQuestion] = useState<QuestionModel>();
   const [questionIds, setQuestionIds] = useState<number[]>([]);
+  const [numberOfCorrectAnswers, setNumberOfCorrectAnswers] = useState<number>(0);
+  const router = useRouter();
+
 
   async function loadQuestionIds() {
     const res = await fetch(`${BASE_URL}/questionario`);
@@ -27,7 +23,6 @@ export default function Home() {
     const res = await fetch(`${BASE_URL}/questoes/${id}`);
     const resQuestion = await res.json();
     setQuestion(QuestionModel.createFromAnObject(resQuestion));
-    console.log(QuestionModel.createFromAnObject(resQuestion))
   }
 
   useEffect(() => {
@@ -38,19 +33,37 @@ export default function Home() {
     questionIds.length > 0 && loadQuestion(questionIds[0])
   }, [questionIds]);
 
-  function chosenAlternative(question: QuestionModel) {
+  function answerTheQuestion(question: QuestionModel) {
+    setQuestion(question);
+    const gotItRight = question.gotItRight;
+    setNumberOfCorrectAnswers(numberOfCorrectAnswers + (gotItRight ? 1 : 0));
+  }
 
+  function getIdOfTheNextQuestion() {
+    if (question) {
+      const indexOfTheNextQuestion = questionIds.indexOf(question.id) + 1;
+      return questionIds[indexOfTheNextQuestion];
+    }
+  }
+
+  function goToTheNextQuestion(idOfTheNextQuestion: number) {
+    loadQuestion(idOfTheNextQuestion);
+  }
+
+  function finish() {
+    router.push({ pathname:"/resultado", query: { total: questionIds.length, corrects: numberOfCorrectAnswers } });
   }
 
   function goToTheNextScenario() {
-
+    const idOfTheNextQuestion = getIdOfTheNextQuestion();
+    idOfTheNextQuestion ? goToTheNextQuestion(idOfTheNextQuestion) : finish();
   }
 
   return (
     <Questionnaire
       question={question}
-      // lastQuestion={true}
-      questionAnswered={chosenAlternative}
+      lastQuestion={getIdOfTheNextQuestion() === undefined}
+      questionAnswered={answerTheQuestion}
       goToTheNextScenario={goToTheNextScenario}
     />
   );
